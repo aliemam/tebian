4d55353f388037e713b7d6ff7ff27298144a4f20
Ali Emam <aliemamhadi@gmail.com>
2017-09-19 23:20:36 +0430

    add more detail

--- src/Api.php
+++ src/Api.php
@@ -67,9 +67,10 @@ class Api
             ]
         ];
         $context = stream_context_create($opts);
-        file_get_contents($url, false, $context);
+        $message = file_get_contents($url, false, $context);
+        $this->res['response'] = $message;
+        $this->res['response_header'] = json_encode($http_response_header);
         $status = explode(' ', $http_response_header[0]);
-        $this->res['response'] = $http_response_header;
         $this->res['code'] = $status;
 
         return $this->res;
